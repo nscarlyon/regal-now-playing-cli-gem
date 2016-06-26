4cbd5860051a0c49b70191033ffc0f222ccd44f9
natasha carlyon <natasha.carlyon@gmail.com>
2016-06-26 22:22:51 +0000

    scraped title, showtimes, genre, and rating

--- lib/regal_now_playing/movie.rb
+++ lib/regal_now_playing/movie.rb
@@ -4,13 +4,28 @@ class RegalNowPlaying::Movie
 
   @@all = []
 
-  def initialize(title = "title", rating = "pg", runtime = "1 hr", genre = "comedy", info = "more info")
+  def initialize(title = "title", rating = "pg", runtime = "1 hr", genre = "comedy", showtimes = "show")
     @title = title
     @rating = rating
     @runtime = runtime
     @genre = genre
-    @info = info
+    @showtimes = showtimes
     @@all << self
+  end
+
+  
+
+  def showtimes=(showtimes)
+    @showtimes = doc.css("time.timeInfo").text
+  end
+
+  def rating_and_runtime
+    #Raiting comes first and then it is seperated by a comma
+    @placeholder = doc.css("div.showtimes-movie-rating-runtime").text
+  end
+
+  def genre=(genre)
+    @genre = div.showtimes-movie-genre
   end
 
   def self.all
--- lib/regal_now_playing/scraper.rb
+++ lib/regal_now_playing/scraper.rb
@@ -2,19 +2,29 @@ require 'pry'
 
 class RegalNowPlaying::Scraper
 
-attr_accessor :title
+attr_accessor :title, :rating
 
-  def self.get_page
-    doc = Nokogiri::HTML(open("http://www.fandango.com/regalmedlockcrossingstadium1826rpx_aamem/theaterpage"))
-  end
+def self.get_page
+  doc = Nokogiri::HTML(open("http://www.fandango.com/regalmedlockcrossingstadium1826rpx_aamem/theaterpage"))
+  # binding.pry
+end
 
   def make_movies
     doc = Nokogiri::HTML(open("http://www.fandango.com/regalmedlockcrossingstadium1826rpx_aamem/theaterpage"))
     title = doc.css("a.dark.showtimes-movie-title").map(&:text)
       title.each_with_index do |t, i|
+      #  RegalNowPlaying::Movie.new_movie("#{t}")
         puts "#{i+1}. #{t}"
       end
+
+      rating = doc.css("div.showtimes-movie-rating-runtime").map(&:text)
+        rating.each do |r|
+        puts "#{r}"
+      end
+
   end
 
+# movie times = doc.css("time.timeInfo").text
+#
 
 end #ends class
